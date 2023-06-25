de7098e4b2de74f0c71eeccd9990541974108ba1
shivam garg <89645660+Shivamg27071999@users.noreply.github.com>
2023-06-25 18:52:32 +0530

    first
    first version of app

---
+++ main.py
@@ -0,0 +1,31 @@
+
+# import phonenumbers
+# # from test import numbers
+# number = "+919779994605"
+
+# from phonenumbers import geocoder
+# ch_numbers = phonenumbers.parse(number,"CH")
+# print(geocoder.description_for_number(ch_numbers,"en"))
+
+# from phonenumbers import carrier
+
+# service_nmbers=phonenumbers.parse(number,"RO")
+# print(carrier.name_for_number(service_nmbers,"en"))
+
+
+import phonenumbers
+# from test import numbers
+import streamlit as st
+number = st.text_input('Enter Number for finding details','Enter here')
+from phonenumbers import geocoder
+ch_numbers = phonenumbers.parse(number,"CH")
+st.write(geocoder.description_for_number(ch_numbers,"en"))
+backgroundColor="white"
+
+from phonenumbers import carrier
+
+service_nmbers=phonenumbers.parse(number,"RO")
+st.write(carrier.name_for_number(service_nmbers,"en"))
+
+
+
